021c6a0cd85acd7950235886ad93cc825dc44933
Moses Kim <moseskim92@yahoo.com>
2016-08-10 19:06:47 -0700

    Done with the project

---
+++ js/controllers.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myController', ['$scope', '$http', function($scope, $http){
+  $http.get('js/data.json').success(function(data) {
+    $scope.contacts = data;
+  });
+}]);
+
